3ccce90ab97a715bcc04b5b5b46e28aab8451adf
Dan Whitaker <dan@uncharted.systems>
2022-05-05 03:42:19 -0600

    p1 part 1 is done in rust

--- p1.rs
+++ p1.rs
@@ -1,19 +1,39 @@
-fn count_increases(nums: [u32; 10])-> i32 {
-    let mut count: i32 = 0;
+use std::fs;
+
+fn count_increases()-> usize {
+
+    let scans: String = fs::read_to_string("i1.txt").expect("oh shit");
+    let parses: Vec<usize> = scans.lines().map(|x| x.parse::<usize>().unwrap()).collect();
+    let mut count: usize = 0;
+    let mut count2: usize = 0;
     let mut index: usize = 0;
 
-    while index < nums.len() - 1 {
-        if nums[index] < nums[(index + 1)] {
+    if 123 < 23 {
+        println!("num true");
+    } else {
+        println!("num false");
+    }
+
+    if "123" < "23" {
+        println!("str true");
+    } else {
+        println!("str false");
+    }
+
+    while index < parses.len() - 1 {
+        if parses[index] < parses[index + 1] {
             count += 1;
+        } else {
+        count2 += 1;
         }
         index += 1;
     }
+    println!("{}", count);
+    println!("{}", count2);
     return count;
 }
 
 
-static scans: [u32; 10] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-
 fn main() {
-    println!("{}", count_increases(scans));
+    println!("{}", count_increases());
 }
