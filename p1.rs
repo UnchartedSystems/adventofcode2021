d92f341205d6e5dd0a0ae374c176e5176c380bc6
Dan Whitaker <dan@uncharted.systems>
2022-05-05 15:05:45 -0600

    finished p1 - is ugly and bad for now

--- p1.rs
+++ p1.rs
@@ -1,39 +1,24 @@
 use std::fs;
 
-fn count_increases()-> usize {
+fn count_increases(span: usize)-> usize {
 
     let scans: String = fs::read_to_string("i1.txt").expect("oh shit");
     let parses: Vec<usize> = scans.lines().map(|x| x.parse::<usize>().unwrap()).collect();
     let mut count: usize = 0;
-    let mut count2: usize = 0;
     let mut index: usize = 0;
 
-    if 123 < 23 {
-        println!("num true");
-    } else {
-        println!("num false");
-    }
-
-    if "123" < "23" {
-        println!("str true");
-    } else {
-        println!("str false");
-    }
-
-    while index < parses.len() - 1 {
-        if parses[index] < parses[index + 1] {
+    while index < parses.len() - span {
+        if parses[index..index + span].iter().sum::<usize>() < parses[index + 1..index + span + 1].iter().sum::<usize>() {
             count += 1;
         } else {
-        count2 += 1;
         }
         index += 1;
     }
-    println!("{}", count);
-    println!("{}", count2);
     return count;
 }
 
 
 fn main() {
-    println!("{}", count_increases());
+    println!("{}", count_increases(1));
+    println!("{}", count_increases(3));
 }
